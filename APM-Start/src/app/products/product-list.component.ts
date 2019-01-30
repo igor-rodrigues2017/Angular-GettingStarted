b806f69a2eca2e2464d123b20b36a81e00b26456
Igor Rodrigues <igor.chagas.rodrigues@gmail.com>
2019-01-30 19:07:07 -0200

    LifeCycle hooks, implementamos uma interface do angular para um ciclo específico

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { IProduct } from "./product";
 
 @Component({
@@ -6,7 +6,7 @@ import { IProduct } from "./product";
     templateUrl: './product-list.component.html',
     styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent{
+export class ProductListComponent implements OnInit{
     pageTitle: string = 'Product List'
     imageWidth: number = 50;
     imageMargin: number = 2;
@@ -21,8 +21,8 @@ export class ProductListComponent{
         "price": 19.95,
         "starRating": 3.2,
         "imageUrl": "https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
-      },
-      {
+    },
+    {
         "productId": 2,
         "productName": "Garden Cart",
         "productCode": "GDN-0023",
@@ -31,10 +31,13 @@ export class ProductListComponent{
         "price": 32.99,
         "starRating": 4.2,
         "imageUrl": "https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-      }];
-
+    }];
+    
     toggleImage(): void{
         this.showImage = !this.showImage;
     }
-
+    
+    ngOnInit(): void {
+        console.log('In OnInit');
+    }
 }
